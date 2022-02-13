4f758268fa8d67d2f05b45bab591e6212a38aaf1
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-02-13 20:44:37 +0000

    pre final sprint 2

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,9 +1,9 @@
 const commentData = [
   {
-    name: "Miles Acosta",
-    date: "12/20/2020",
+    name: "Connor Walton",
+    date: "02/17/2021",
     comment:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
   },
   {
     name: "Emilie Beach",
@@ -12,10 +12,10 @@ const commentData = [
       "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
   },
   {
-    name: "Connor Walton",
-    date: "02/17/2021",
+    name: "Miles Acosta",
+    date: "12/20/2020",
     comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
   },
 ];
 
